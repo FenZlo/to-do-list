28915243b328d280c05e03a446092d7033a75b61
FenZlo <daniel.uporov@gmail.com>
2022-11-03 18:54:35 +0500

    Changes with JS

--- script.js
+++ script.js
@@ -1,13 +1,25 @@
-const elementButton = document.getElementById('botton');
-let elementAddString = document.getElementsById('addInput');
+const elementButton = document.querySelector('button');
+let elementAddString = document.getElementById('addInput');
 let toDoList = document.getElementsByClassName('list')[0];
 
 elementButton.addEventListener('click', clickHandler); //добавляю событие на клик по кнопке
 function clickHandler (event) {
     let newElement = document.createElement('div'); //создаю новый элемент, чтобы добавить его в список
-    div.className = 'newElements'; //присваиваю класс новому элементу, чтобы изменять его свойства в CSS
-    let elementAddString = document.getElementsById('addInput').value; //присваиваю новой переменной значение, которое ввел в поле ввода
-    newElement.appendChild(elementAddString); //добавляю значение из поля ввода в div, который создал ранее
-    toDoList.appendChild(newElemetnt); //Перемещаю div в список дел, который создал заранее в HTML и объявил его в начале JS
+    newElement.classList.add('newElements'); //присваиваю класс новому элементу, чтобы изменять его свойства в CSS
+    
+    let textOfBusiness = document.createElement('div')
+    
+    let elementAddString = document.getElementById('addInput').value; //присваиваю новой переменной значение, которое ввел в поле ввода
     if (elementAddString.lenth<0) return; //Проверка на то, что поле ввода не пустое
+    console.log(elementAddString);
+    
+    textOfBusiness.appendChild(elementAddString); //добавляю значение из поля ввода в newElement, который создал ранее
+    textOfBusiness.appendChild(newElement);
+    
+    let trash = document.createElement('img')
+    trash.src = './to-do-list/trash.png'
+    newElement.appendChild(trash);
+    
+    toDoList.appendChild(newElement); //Перемещаю div в список дел, который создал заранее в HTML и объявил его в начале JS
+    
 }
